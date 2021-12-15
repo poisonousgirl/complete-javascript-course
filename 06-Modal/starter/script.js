8e7a0f806edcf040a25de79c61f12d61d5725336
Anna <gravillon.anna@gmail.com>
2021-12-15 12:41:17 +0100

    modal buttons

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -1,1 +1,31 @@
 'use strict';
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const buttonCloseModal = document.querySelector('.close-modal');
+const buttonsOpenModal = document.querySelectorAll('.show-modal');
+
+const openModal = () => {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+
+const closeModal = () => {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+for (let i = 0; i < buttonsOpenModal.length; i++)
+  buttonsOpenModal[i].addEventListener('click', openModal);
+
+buttonCloseModal.addEventListener('click', closeModal);
+
+overlay.addEventListener('click', closeModal);
+
+document.addEventListener('keydown', evt => {
+  if (
+    evt.key === 'Escape' ||
+    (evt.key === 'Esc' && !modal.classList.contains('hidden'))
+  ) {
+    closeModal();
+  }
+});
